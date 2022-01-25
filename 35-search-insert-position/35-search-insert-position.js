7fd9d8cdc118ba0ffec0a9127d95eaa8f940bfef
Niraj Patel <Niraj2323@gmail.com>
2022-01-25 18:14:52 -0500

    Time: 84 ms (44.90%), Space: 39.6 MB (63.03%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while (start <= end) {
+        let mid = Math.floor((start+end)/2);
+        if (target > nums[mid]) {
+            start = mid+1;
+        } else {
+            end = mid-1;
+        }
+    }
+    
+    return start;
+};
