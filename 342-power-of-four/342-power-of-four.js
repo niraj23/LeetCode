1a1dc5cab464692880dcee78adf436f490ebdd13
Niraj Patel <Niraj2323@gmail.com>
2022-02-04 18:43:24 -0500

    Time: 165 ms (5.99%), Space: 43.8 MB (5.59%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
-         if(n <= 0)
-            return false; 
-        if(n==1)
-            return true;
-        return n%4 == 0 && isPowerOfFour(n/4);
-    }
+    if (n < 1) return false
+    while (n > 1){
+       n = n / 4
+   }
+    return n === 1
+};
