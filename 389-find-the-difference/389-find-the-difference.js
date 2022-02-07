d17b52e820eb45ba4326159317cad7486002f60d
Niraj Patel <Niraj2323@gmail.com>
2022-02-06 22:03:08 -0500

    Time: 110 ms (42.09%), Space: 42.4 MB (24.09%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let code = 0;
+    for(let i=0; i<t.length; i++){
+        code += t.charCodeAt(i);
+        if(i < s.length){
+            code -= s.charCodeAt(i);
+        }
+    }
+    return String.fromCharCode(code);
+};
