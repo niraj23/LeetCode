ebd1fffd9e93a52777051b653fa9617a47ccb161
Niraj Patel <Niraj2323@gmail.com>
2022-03-28 23:43:02 -0400

    Time: 58 ms (92.68%), Space: 42.1 MB (59.25%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+     let lastInteger = digits[digits.length-1]
+ 
+ if (lastInteger < 9) digits[digits.length-1]++
+ else {
+    let pointer = digits.length-1
+    while (digits[pointer] == 9) {
+        digits[pointer] = 0
+        pointer--
+    }
+     if (pointer>=0) digits[pointer]++
+     else digits.unshift(1)
+ }
+    return digits
+};
